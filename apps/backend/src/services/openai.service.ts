13b8d7be726728f2de40b27dfdf243f81d0fe38a
Rai <62800649+menamerai@users.noreply.github.com>
2024-06-29 16:48:37 -0700

    OpenAI prompt change (#1)
    * changed prompt for form
    
    * prompt tweak

--- apps/backend/src/services/openai.service.ts
+++ apps/backend/src/services/openai.service.ts
@@ -10,14 +10,19 @@ export class OpenaiService {
 
     const prompt = `
                     Analyze the image provided. The image MUST satisfy all of the following criteria:
-                        1. It must have as subject a receipt of purchase of at least one product.
+                        1. It must have as subject a form of proof of donation from an NGO or a charity.
                         2. It must not be a screenshot.
-                        3. It must include the date of the purchase.
-                        4. It must include the name of the store where the purchase was made.
+                        3. It must include the date of the purchase in the "Donation Date" field.
+                        4. It must include the declared value of the donation in the "Declared Value" field.
+                        5. The box for "Food described as" must be ticked, crossed or checked and the description must be filled with some form of food.
+                        6. There must exist a signature below the "Sincerely," text at the end of the form.
                     Please respond using a JSON object without comments and do not add any other descriptions and comments:
                     {
                     'validityFactor': number, // 0-1, 1 if it satisfies all the criteria, 0 otherwise
-                    'descriptionOfAnalysis': string, // indicate your analysis of the image and why it satisfies or not the criteria. The analysis will be shown to the user so make him understand why the image doesn't satisfy the criteria if it doesn't without going into detail on exact criteria. Remember we are rewarding users that drink coffee in a sustainable way.
+                    'descriptionOfAnalysis': string, // indicate your analysis of the image and why it satisfies or not the criteria. The analysis will be shown to the user so make them understand why the image doesn't satisfy the criteria if it doesn't without going into detail on exact criteria. Remember we are rewarding users for donation of food to the needy.
+                    'donationValue': number, // the declared value of the donation
+                    'donationDate': string, // the date of the donation, format: YYYY-MM-DD
+                    'foodDescription': string, // the description of the food
                     }
                     `;
 

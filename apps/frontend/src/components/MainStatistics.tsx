f11a4a876511f5d2193da4386150785e8e3545be
audgeviolin07 <audgeviolin07@gmail.com>
2024-06-29 21:17:08 -0700

    hopefully

--- apps/frontend/src/components/MainStatistics.tsx
+++ apps/frontend/src/components/MainStatistics.tsx
@@ -3,16 +3,20 @@ import { Container, Stat, StatLabel, StatNumber, StatHelpText, StatGroup } from 
 export const MainStatistics = () => {
     return (
         <Container
-            textColor={"white"}
-            width={"100%"}
+            mt={{ base: 4, md: 10 }}
+            maxW={"container.xl"}
+            mb={{ base: 4, md: 10 }}
+            display={"flex"}
+            flex={1}
+            alignItems={"flex-center"} // Align items to the left
+            justifyContent={"flex-center"}
+            flexDirection={"column"}
         >
             {
             // use the useConnex hook to grab data from the blockchain
             }
 
-            <StatGroup
-                textAlign={"center"}
-            >
+            <StatGroup>
                 <Stat>
                     <StatLabel>Files Uploaded</StatLabel>
                     <StatNumber>345</StatNumber>
